be9b1f207a01b1e325956bdac8c2bdd1865714ae
nao7009 <shimojounao@shimojounaonoMacBook-Air.local>
2024-01-12 21:42:06 +0900

    docsフォルダ追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const path = require('path');
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
   // add
-  app: {
-    baseURL: '/'
-  }
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, '/docs')
+    }
+  },
 })
