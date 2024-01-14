15c3e45e038299f08b8dac07aeb0e9964c83953b
nao7009 <shimojounao@shimojounaonoMacBook-Air.local>
2024-01-14 15:02:46 +0900

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-// const path = require('path');
+const path = require('path');
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
   // add
-  // nitro: {
-  //   output: {
-  //     publicDir: path.join(__dirname, '/docs')
-  //   }
-  // },
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, '/docs')
+    }
+  },
 
-  // app: {
-  //   baseURL: '/calorie-app',
-  // },
+  app: {
+    baseURL: '/calorie-app',
+  },
 })
